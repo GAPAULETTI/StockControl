584553cf139ab333d9ec6607b9eed5e19e60f6bc
gpauletti <gastonpauletti@gmail.com>
2024-01-25 12:57:56 -0300

    B1-ConnectMySql

--- src/main/java/com/gypdev/stock/StockApplication.java
+++ src/main/java/com/gypdev/stock/StockApplication.java
@@ -1,7 +1,12 @@
 package com.gypdev.stock;
 
+import com.gypdev.stock.model.Product;
+import com.gypdev.stock.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StockApplication {
@@ -10,4 +15,13 @@ public class StockApplication {
 		SpringApplication.run(StockApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(ProductRepository repoProduct){
+
+
+		return (args -> {
+			Product product1 = new Product("Escoba", "Larga|Sintetica", 99.0, 10);
+			repoProduct.save(product1);
+		});
+	}
 }
--- src/main/java/com/gypdev/stock/model/Inventory.java
+++ src/main/java/com/gypdev/stock/model/Inventory.java
@@ -1,11 +1,15 @@
 package com.gypdev.stock.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Entity
+
 public class Inventory {
 
     private Map<String, Product> productMap;
@@ -14,8 +18,18 @@ public class Inventory {
         productMap = new HashMap<>();
     }
 
-    public void addProduct(String name,String description, Double price, int stock){
+
+
+    public void addProduct(String name, String description, Double price, int stock){
         Product product = new Product(name, description, price, stock);
         productMap.put(name, product);
+    }
+    public void updateStock(String productName, int quantity){
+        Product product = productMap.get(productName);
+        if (product != null){
+            product.updateStock(quantity);
+        } else {
+            System.out.println("Product not found: " + productName);
+        }
     }
 }
