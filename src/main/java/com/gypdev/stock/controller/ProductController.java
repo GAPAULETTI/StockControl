64eb36119f89377467a7572d2c44d74800a11051
gpauletti <gastonpauletti@gmail.com>
2024-02-08 17:43:45 -0300

    Add Products-Html-V3

--- src/main/java/com/gypdev/stock/controller/ProductController.java
+++ src/main/java/com/gypdev/stock/controller/ProductController.java
@@ -5,10 +5,10 @@ import com.gypdev.stock.DTO.ProductDTO;
 import com.gypdev.stock.model.Product;
 import com.gypdev.stock.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +26,17 @@ public class ProductController {
     @GetMapping("/products/{id}")
     public ProductDTO getProductById(@PathVariable Long id){
         return repoProduct.findById(id).map(product -> new ProductDTO(product)).orElse(null);
+    }
+    @Transactional
+    @PostMapping("/products")
+    public ResponseEntity<Object> updateStockProduct(@RequestParam int quantity, Product product) {
+        if (quantity >= 0) {
+            product.setStock(quantity);
+            return new ResponseEntity<>("The product is update", HttpStatus.ACCEPTED);
+        }else {
+            return new ResponseEntity<>("An error has occurred", HttpStatus.NOT_MODIFIED);
+        }
+
+
     }
 }
